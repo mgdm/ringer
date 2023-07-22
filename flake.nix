1d059456dd73373fad962e67272c15911b2b5642
Michael Maclean <michael@mgdm.net>
2023-07-22 22:22:28 +0100

    Attempt at a NixOS module?

--- flake.nix
+++ flake.nix
@@ -12,10 +12,63 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         naersk' = pkgs.callPackage naersk { };
+        ringer = naersk'.buildPackage { src = ./.; };
       in rec {
-        defaultPackage = naersk'.buildPackage { src = ./.; };
+        packages.ringer = ringer;
+
+        defaultPackage = packages.ringer;
 
         devShell =
           pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo ]; };
-      });
+
+      }) // {
+        nixosModules = {
+          ringer = { config, lib, ringer, ... }:
+            let cfg = config.services.ringer.enable;
+            in rec {
+              options = {
+                enable = lib.mkOption {
+                  default = true;
+                  example = true;
+                  type = lib.types.bool;
+
+                  description = ''
+                    Enable ringer service
+                  '';
+                };
+
+                config = lib.mkIf config.enable {
+                  systemd.sockets.ringer = {
+                    enable = true;
+                    wantedBy = [ "sockets.target" ];
+                    socketConfig = {
+                      ListenStream = 79;
+                      Accept = "yes";
+                    };
+                  };
+
+                  systemd.services."ringer@" = {
+                    enable = true;
+                    description = "Ringer service";
+                    requires = [ "ringer.socket" ];
+
+                    serviceConfig = {
+                      DynamicUser = "yes";
+                      ProtectHome = "true";
+                      ExecStart = "-${ringer}/bin/fingerd";
+                      StandardInput = "socket";
+                      StandardOutput = "socket";
+                      StateDirectory = "ringer";
+                    };
+                  };
+                };
+              };
+            };
+
+          default = { self, pkgs, ... }: {
+            imports = [ self.nixosModules.ringer ];
+            services.ringer = { enable = true; };
+          };
+        };
+      };
 }
